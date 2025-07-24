014cd58c40b973e93a9725bc4504e33360ab304f
charleslang404 <charleslang404@gmail.com>
2025-07-24 04:03:02 +0000

    new file:   capacitor.config.ts 	modified:   package-lock.json 	modified:   package.json

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.OfficiaX1.app',
+  appName: 'OfficiaX',
+  webDir: 'public'
+};
+
+export default config;
